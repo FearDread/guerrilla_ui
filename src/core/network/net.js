7ddbce284b7cb27826b92159342162dd746da306
ghaptonstall <ghaptonstall@gmail.com>
2015-05-23 23:29:16 -0700

    --update net module script method

--- src/core/network/net.js
+++ src/core/network/net.js
@@ -5,26 +5,41 @@ $.GUI().create('Net', function(GUI){
     var Model = GUI.Model,
         Net = {
 
-        post:function(){
+            post:function(){
 
-        },
+            },
 
-        get:function(){
+            get:function(){
         
-        },
+            },
 
-        put:function(){
+            put:function(){
         
-        },
+            },
 
-        del:function(){
+            del:function(){
         
-        },
+            },
 
-        script:function(){
+            script:function(url, data, callback){
+
+                $.ajax({url:url, type:'script', data:data,
+                    success:function(resp){
+                        GUI.log('script resp = ', resp);
+
+                        if(resp && resp.status == 200){
+                            Model.create(url, resp);
+                            callback(resp);
+                        }
+                
+                    },
+                    error:function(error){
+                        GUI.log('Error :: ', error);
+                    }
+                })
         
-        }
-    };
+            }
+        };
 
     function _unload(){
         GUI.log('Resetting Module Class');
