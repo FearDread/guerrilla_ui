74764a326d0c41131f22c5a64831adba83734f26
FearDread <ghaptonstall@gmail.com>
2015-09-05 00:41:12 -0700

    --remove un-needed module and plugin files and old core object. --update sandbox api with needed methods from old core, hitch, methodCache, and query.

--- src/api.js
+++ src/api.js
@@ -3,19 +3,94 @@
 * @author: Garrett Haptonstall (FearDread) *
 * @module: $.GUI Sandbox API               * 
 * ---------------------------------------- */
-var API = function() {
+var API;
+
+API = function() {
+
+    // API Globals
+    var DELIM = '__';
 
     return {
+        // create new API sandbox instance
         create: function(core, instance, options, module) {
 
+            // set sandbox vars
             this.id = instance;
             this.module = module;
             this.options = (options !== null) ? options : {}; 
 
+            // add utils object
             this.utils = utils;
 
             // attach new sandbox instance
             core._broker.install(this);
+
+            this.elem = function(el) {
+                if (!utils.isStr(el)) {
+
+                    core.log('Error :: Element must be type String.');
+
+                    return false;
+                }
+
+                return document.createElement(el);
+            };
+
+            this.query = function(selector, context) {
+                var $el, _ret = {}, _this = this;
+                
+                // check for applied context
+                if (context && context.find) {
+                    // use dom find
+                    $el = context.find(selector);
+
+                } else {
+                    // wrap with jQuery
+                    $el = $(selector);
+                }
+
+                // set retainer object
+                _ret = $el;
+                _ret.length = $el.length;
+
+                _ret.query = function(sel) {
+
+                    return _this.query(sel, $el);
+                };
+
+                return _ret;
+            };
+
+            this.hitch = function(fn) {
+                var argc, all;
+
+                argc = [].slice.call(arguments).splice(1);
+
+                return function() {
+                    all = argc.concat([].slice.call(arguments));
+
+                    return fn.apply(this, all);
+                };
+            };
+
+            this.methodCache = function(source, cache, refetch) {
+                var key;
+
+                cache || (cache = {});
+
+                return function(args) {
+                    key = arguments.length > 1 ? [].join.call(arguments, DELIM) : String(args);
+
+                    if (!(key in cache) || (refetch && cache[key] === refetch)) {
+
+                        cache[key] = source.apply(source, arguments);
+
+                    }
+
+                    return cache[key];
+                }
+
+            };
 
             return this;
         }
