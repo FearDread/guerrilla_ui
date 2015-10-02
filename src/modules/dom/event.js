48b373eb17dfc6fb8ba1e4da08fd3344c3ea3beb
FearDread <ghaptonstall@gmail.com>
2015-10-01 23:48:27 -0700

    --remove un-needed dom/dom.js file and update new dom/event and dom/map modules.

--- src/modules/dom/event.js
+++ src/modules/dom/event.js
@@ -4,95 +4,127 @@
 * ---------------------------------------- */
 $.GUI().use(function(gui) {
 
+    Event = (function() {
+
+        function Event() {}
+
+        Event.prototype.extend = function(custom, defaults) {
+            var key, value;
+
+            for (key in defaults) {
+                value = defaults[key];
+
+                if (custom[key] === null) {
+
+                    custom[key] = value;
+                }
+            }
+
+            return custom;
+        };
+
+        Event.prototype.isMobile = function(agent) {
+            return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(agent);
+        };
+
+        Event.prototype.create = function(event, bubble, cancel, detail) {
+            var customEvent;
+
+            if (!bubble || bubble === null) {
+                bubble = false;
+            }
+
+            if (!cancel || cancel === null) {
+                cancel = false;
+            }
+
+            if (!detail || detail === null) {
+                detail = null;
+            }
+
+            if (document.createEvent !== null) {
+
+                customEvent = document.createEvent('CustomEvent');
+                customEvent.initCustomEvent(event, bubble, cancel, detail);
+
+            } else if (document.createEventObject != null) {
+
+                customEvent = document.createEventObject();
+                customEvent.eventType = event;
+
+            } else {
+
+                customEvent.eventName = event;
+            }
+
+            return customEvent;
+        };
+
+        Event.prototype.fire = function(elem, event) {
+            if (elem.dispatchEvent && elem.dispatchEvent !== null) {
+
+                return elem.dispatchEvent(event);
+
+            } else if (event in (elem !== null)) {
+
+                return elem[event]();
+
+            } else if (("on" + event) in (elem !== null)) {
+
+                return elem["on" + event]();
+            }
+        };
+
+        Event.prototype.add = function(elem, event, fn) {
+            if (elem.addEventListener !== null) {
+
+                return elem.addEventListener(event, fn, false);
+
+            } else if (elem.attachEvent !== null) {
+
+                return elem.attachEvent("on" + event, fn);
+
+            } else {
+
+                return elem[event] = fn;
+            }
+        };
+
+        Event.prototype.remove = function(elem, event, fn) {
+            if (elem.removeEventListener !== null) {
+
+                return elem.removeEventListener(event, fn, false);
+
+            } else if (elem.detachEvent !== null) {
+
+                return elem.detachEvent("on" + event, fn);
+
+            } else {
+
+                return delete elem[event];
+            }
+        };
+
+        Event.prototype.innerHeight = function() {
+            if ('innerHeight' in window) {
+
+                return window.innerHeight;
+
+            } else {
+
+                return document.documentElement.clientHeight;
+            }
+        };
+
+        return Event;
+
+    })();
+
     return {
         
         load: function(api) {
 
-Event = (function() {
-    function Event() {}
-
-    Event.prototype.extend = function(custom, defaults) {
-      var key, value;
-      for (key in defaults) {
-        value = defaults[key];
-        if (custom[key] == null) {
-          custom[key] = value;
-        }
-      }
-      return custom;
-    };
-
-    Event.prototype.isMobile = function(agent) {
-      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(agent);
-    };
-
-    Event.prototype.createEvent = function(event, bubble, cancel, detail) {
-      var customEvent;
-      if (bubble == null) {
-        bubble = false;
-      }
-      if (cancel == null) {
-        cancel = false;
-      }
-      if (detail == null) {
-        detail = null;
-      }
-      if (document.createEvent != null) {
-        customEvent = document.createEvent('CustomEvent');
-        customEvent.initCustomEvent(event, bubble, cancel, detail);
-      } else if (document.createEventObject != null) {
-        customEvent = document.createEventObject();
-        customEvent.eventType = event;
-      } else {
-        customEvent.eventName = event;
-      }
-      return customEvent;
-    };
-
-    Event.prototype.emitEvent = function(elem, event) {
-      if (elem.dispatchEvent != null) {
-        return elem.dispatchEvent(event);
-      } else if (event in (elem != null)) {
-        return elem[event]();
-      } else if (("on" + event) in (elem != null)) {
-        return elem["on" + event]();
-      }
-    };
-
-    Event.prototype.addEvent = function(elem, event, fn) {
-      if (elem.addEventListener != null) {
-        return elem.addEventListener(event, fn, false);
-      } else if (elem.attachEvent != null) {
-        return elem.attachEvent("on" + event, fn);
-      } else {
-        return elem[event] = fn;
-      }
-    };
-
-    Event.prototype.removeEvent = function(elem, event, fn) {
-      if (elem.removeEventListener != null) {
-        return elem.removeEventListener(event, fn, false);
-      } else if (elem.detachEvent != null) {
-        return elem.detachEvent("on" + event, fn);
-      } else {
-        return delete elem[event];
-      }
-    };
-
-    Event.prototype.innerHeight = function() {
-      if ('innerHeight' in window) {
-        return window.innerHeight;
-      } else {
-        return document.documentElement.clientHeight;
-      }
-    };
-
-    return Event;
-
-  })();
-
-            api.dom.event = Event;
-
+            api.dom.event = new Event();
         },
         unload: function() {}
     };
---
+++ src/modules/dom/map.js
@@ -0,0 +1,60 @@
+/* --------------------------------------- *
+* Guerrilla UI                             *
+* @module: Map, basic key value map store  * 
+* ---------------------------------------- */
+$.GUI().use(function(gui) {
+
+    Map = gui.config.win.Map || gui.config.win.MozMap || (Map = (function() {
+
+        function Map() {
+            this.keys = [];
+            this.values = [];
+        }
+
+        Map.prototype.get = function(key) {
+            var i, item, j, ref;
+
+            ref = this.keys;
+
+            for (i = j = 0; j < ref.length; i = ++j) {
+                item = ref[i];
+
+                if (item === key) {
+
+                    return this.values[i];
+                }
+            }
+        };
+
+        Map.prototype.set = function(key, value) {
+          var i, item, j, ref;
+
+          ref = this.keys;
+
+          for (i = j = 0; j < ref.length; i = ++j) {
+              item = ref[i];
+
+              if (item === key) {
+                  this.values[i] = value;
+                  return;
+              }
+          }
+
+          this.keys.push(key);
+
+          return this.values.push(value);
+        };
+
+        return Map;
+
+    })());
+
+    return {
+
+        load: function(api) {
+
+          api.dom.map = new Map();
+        },
+        unload: function() {}
+    };
+});
