3b64fbd3ce46020166ce96d8e9bdb7582cc35559
FearDread <ghaptonstall@gmail.com>
2015-10-05 00:28:54 -0700

    --add new jQuery plugins/background plugin with needed logic.

---
+++ src/plugins/background.js
@@ -0,0 +1,38 @@
+
+$.GUI().create('background', function(gui) {
+
+    return {
+        fn: function($el, options) {
+            var _this = this, defaults, settings;
+
+            defaults = {
+                afterLoaded: function() {
+                    this.addClass('bg-loaded');
+                }
+            };
+            settings = gui.utils.merge({}, defaults, options);
+
+            gui.each(function() {
+                var $this = gui.$(this), bgImages;
+
+                bgImages = window.getComputedStyle($this.get(0), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "").split(', ');
+
+                $this.data('loaded-count', 0);
+
+                gui.each(bgImages, function(key, value) {
+                    var img = value.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
+
+                    gui.$('<img/>').attr('src', img).load(function() {
+                        $(this).remove(); // prevent memory leaks
+
+                        $this.data('loaded-count', $this.data('loaded-count') + 1);
+
+                        if ($this.data('loaded-count') >= bgImages.length) {
+                            settings.afterLoaded.call($this);
+                        }
+                    });
+                });
+            });
+        }
+    };
+}).start('background');
