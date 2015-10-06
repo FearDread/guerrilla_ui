cce6b943c50ad4364ed1d646d3f9cfc1adb9a96d
FearDread <ghaptonstall@gmail.com>
2015-10-06 16:52:22 -0700

    --update Gruntfile.js config and add new test.js grunt task via build/tasks dir.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,66 @@
 /* --------------------------------------- *
 * Gurilla JS                               *
-* @author: Garrett Haptonstall (FearDread) *
-* @module: Grunt CLI build & config        * 
+* @module: Grunt CLI build                 * 
 * ---------------------------------------- */
 module.exports = function(grunt){
+    // requireds
+    require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
-      pkg:grunt.file.readJSON('package.json'),
-      concat: {
-          options: {
-              separator: ';'
-          },
-          dist: {
-              src: [
-                  'src/utils.js',
-                  'src/broker.js',
-                  'src/api.js',
-                  'src/GUI.js',
-                  'src/plugin.js',
-                  'src/modules/dom/map.js',
-                  'src/modules/dom/event.js',
-                  'src/modules/mvc/model.js',
-                  'src/modules/mvc/view.js',
-                  'src/modules/mvc/controller.js',
-                  'src/modules/network/router.js',
-                  'src/modules/ui/media.js',
-                  'src/modules/ui/charm.js',
-                  'src/modules/lang/lang.js',
-                  'src/modules/lang/array.js',
-                  'src/modules/lang/object.js',
-                  'src/modules/lang/function.js',
-                  'src/modules/util/cellar.js',
-                  'src/modules/util/cookie.js',
-                  'src/plugins/background.js',
-                  'src/plugins/layers.js',
-                  'src/plugins/stargaze.js',
-                  'src/plugins/misty.js',
-              ],
-              dest: 'dist/<%= pkg.name %>.js'
-           }
-      },
-      uglify: {
-          options: {
-              preserveComments: false,
-              banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-          },
-          dist: {
-              files: {
-                  'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-              }
-          }
-      }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg:grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: [
+                    'src/utils.js',
+                    'src/broker.js',
+                    'src/api.js',
+                    'src/GUI.js',
+                    'src/plugin.js',
+                    'src/modules/dom/map.js',
+                    'src/modules/dom/event.js',
+                    'src/modules/mvc/model.js',
+                    'src/modules/mvc/view.js',
+                    'src/modules/mvc/controller.js',
+                    'src/modules/network/router.js',
+                    'src/modules/ui/media.js',
+                    'src/modules/ui/charm.js',
+                    'src/modules/lang/lang.js',
+                    'src/modules/lang/array.js',
+                    'src/modules/lang/object.js',
+                    'src/modules/lang/function.js',
+                    'src/modules/util/cellar.js',
+                    'src/modules/util/cookie.js',
+                    'src/plugins/background.js',
+                    'src/plugins/layers.js',
+                    'src/plugins/stargaze.js',
+                    'src/plugins/misty.js',
+                ],
+                dest: 'dist/<%= pkg.name %>.js'
+             }
+        },
+        uglify: {
+            options: {
+                preserveComments: false,
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  
-  grunt.registerTask('default', ['concat','uglify']);
+    // load external tasks
+    grunt.loadTasks('build/tasks');
+    
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('build', ['test', 'concat','uglify']);
 };
---
+++ build/tasks/tests.js
@@ -0,0 +1,14 @@
+/* --------------------------------------- *
+* Gurilla JS                               *
+* @module: Grunt jsHint test               * 
+* ---------------------------------------- */
+module.exports = function(grunt) {
+
+    grunt.config(['jshint'], {
+        all: ['Gruntfile.js', 'src/**/*.js']
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+  
+    grunt.registerTask('test', ['jshint']);
+};
