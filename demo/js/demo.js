eb28b2a0bff9b4137d1740e8a9ee00f2f6c96f0f
FearDread <ghaptonstall@gmail.com>
2015-10-08 22:50:02 -0700

    --update demo js logic and handlers and update demo page html and css.

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -5,13 +5,34 @@
 * ---------------------------------------- */
 $.GUI().create('App', function(gui) {
     console.log('Demo app :: ', gui);
+    var events;
 
-    var navHandler = function(data) {
-        gui.log('nav handler event fired.');
-        gui.log('data = ', data);
+    events = {
+        scroll: function(e) {
+            e.preventDefault();
+            gui.fire('scrollPage');
+        },
+        navclick: function(data) {
+            gui.log('nav handler event fired.');
+            gui.log('data = ', data);
+        },
+        showPage: function(e) {
+            e.preventDefault();
+
+            var page, href, target;
+
+            target = gui.$(e.target);
+
+            if (target && target.is('a')) {
+                href = target.attr('href').split('#')[1];
+            }
+
+            gui.fire('navclick', {page: href}, function() {
+                gui.log('callback after fired navclick event.');
+                gui.net.router.navigate();
+            });
+        }
     };
-
-    gui.add('navclick', navHandler); 
 
     function _load(opts) {
         var mediaListener;
@@ -19,10 +40,11 @@ $.GUI().create('App', function(gui) {
         gui.add('scrollPage', function() {
             console.log('fired scroll page channel');
         });
+        gui.add('navclick', events.navclick);
 
         initView();
 
-        gui.net.router.config({ mode: 'hash'});
+        gui.net.router.config({ mode: 'history'});
 
         gui.net.router.add(/about/, function() {
             console.log('about');
@@ -44,17 +66,27 @@ $.GUI().create('App', function(gui) {
         mediaListener();
 
         new gui.ui.charm().init();
+
+        var scrollAnimationTime = 1200,
+            scrollAnimation = 'easeInOutExpo';
+
+            $('.btn').bind('click.smoothscroll', function (event) {
+                event.preventDefault();
+                var target = this.hash;
+
+                $('html, body').stop().animate({
+
+                    'scrollTop': $(target).offset().top + 60
+
+                }, scrollAnimationTime, scrollAnimation, function () {
+                    window.location.hash = target;
+                });
+            });
     }
 
     function bindEvents() {
-        var scroll = function(e) {
-            e.preventDefault();
-            gui.fire('scrollPage');
-        };
-
-        gui.$('.navbar-top').click(showPage);
-
-        gui.$('.btn').click(scroll);
+        gui.$('.navbar-top').click(events.showPage);
+        gui.$('.btn').click(events.scroll);
     }
 
     function initView() {
@@ -74,23 +106,11 @@ $.GUI().create('App', function(gui) {
         bindEvents();
     }
 
-    function showPage(e) {
-        var page, href, target;
-        e.preventDefault();
-
-        target = gui.$(e.target);
-
-        if (target && target.is('a')) {
-            href = target.attr('href').split('#')[1];
-        }
-
-        gui.fire('navclick', {page: href}, function() {
-            // gui.net.router.navigate();
-            gui.log('callback after fired navclick event.');
-        });
-    }
-
     return {
-        load: _load,
+        load: function() {
+
+            _load();
+        },
+        unload: function() {}
     };
 }).start('App');
