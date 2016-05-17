562862ad43d8233f8e283587a3e4ded6ca47822d
Garrett Haptonstall <ghaptonstall@gmail.com>
2016-05-16 17:42:36 -0700

    --fix minor logic and syntax in demo page and switch to misty plugin

--- src/plugins/jquery/misty.js
+++ src/plugins/jquery/misty.js
@@ -4,62 +4,40 @@
 * ---------------------------------------- */
 $.GUI().create('Misty', function(G) {
 
-    // Create an array to store our particles
     var particles = [];
-
-    // The amount of particles to render
     var particleCount = 30;
-
-    // The maximum velocity in each direction
     var maxVelocity = 2;
-
-    // The target frames per second (how often do we want to update / redraw the scene)
     var targetFPS = 33;
-
-    // Set the dimensions of the canvas as variables so they can be used.
     var canvasWidth = 400;
     var canvasHeight = 400;
-
-    // Create an image object (only need one instance)
     var imageObj = new Image();
 
-    // Once the image has been downloaded then set the image on all of the particles
     imageObj.onload = function() {
         particles.forEach(function(particle) {
                 particle.setImage(imageObj);
         });
     };
 
-    // Once the callback is arranged then set the source of the image
     imageObj.src = "img/fog.png";
 
-    // A function to create a particle object.
     function Particle(context) {
 
-        // Set the initial x and y positions
         this.x = 0;
         this.y = 0;
 
-        // Set the initial velocity
         this.xVelocity = 0;
         this.yVelocity = 0;
 
-        // Set the radius
         this.radius = 5;
-
-        // Store the context which will be used to draw the particle
         this.context = context;
 
-        // The function to draw the particle on the canvas.
         this.draw = function() {
-            
-            // If an image is set draw it
+
             if (this.image) {
                 this.context.drawImage(this.image, this.x-128, this.y-128);         
-                // If the image is being rendered do not draw the circle so break out of the draw function                
                 return;
             }
-            // Draw the circle as before, with the addition of using the position and the radius from this object.
+
             this.context.beginPath();
             this.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
             this.context.fillStyle = "rgba(0, 255, 255, 0)";
@@ -67,30 +45,25 @@ $.GUI().create('Misty', function(G) {
             this.context.closePath();
         };
 
-        // Update the particle.
         this.update = function() {
-            // Update the position of the particle with the addition of the velocity.
             this.x += this.xVelocity;
             this.y += this.yVelocity;
 
-            // Check if has crossed the right edge
             if (this.x >= canvasWidth) {
                 this.xVelocity = -this.xVelocity;
                 this.x = canvasWidth;
             }
-            // Check if has crossed the left edge
+
             else if (this.x <= 0) {
                 this.xVelocity = -this.xVelocity;
                 this.x = 0;
             }
 
-            // Check if has crossed the bottom edge
             if (this.y >= canvasHeight) {
                 this.yVelocity = -this.yVelocity;
                 this.y = canvasHeight;
             }
             
-            // Check if has crossed the top edge
             else if (this.y <= 0) {
                 this.yVelocity = -this.yVelocity;
                 this.y = 0;
@@ -114,15 +87,12 @@ $.GUI().create('Misty', function(G) {
         };
     }
 
-    // A function to generate a random number between 2 values
     function generateRandom(min, max) {
         return Math.random() * (max - min) + min;
     }
 
-    // The canvas context if it is defined.
     var context;
 
-    // Initialise the scene and set the context if possible
     function init() {
         var i, particle, canvas;
         
@@ -130,17 +100,13 @@ $.GUI().create('Misty', function(G) {
 
         if (canvas.getContext) {
 
-            // Set the context variable so it can be re-used
             context = canvas.getContext('2d');
 
-            // Create the particles and set their initial positions and velocities
             for (i = 0; i < particleCount; i++) {
                 particle = new Particle(context);
                 
-                // Set the position to be inside the canvas bounds
                 particle.setPosition(generateRandom(0, canvasWidth), generateRandom(0, canvasHeight));
                 
-                // Set the initial velocity to be either random and either negative or positive
                 particle.setVelocity(generateRandom(-maxVelocity, maxVelocity), generateRandom(-maxVelocity, maxVelocity));
                 particles.push(particle);            
             }
@@ -149,18 +115,14 @@ $.GUI().create('Misty', function(G) {
         }
     }
 
-    // The function to draw the scene
     function draw() {
-        // draw clear canvas 
         context.clearRect(0,0,canvasWidth,canvasHeight);
 
-        // Go through all of the particles and draw them.
         particles.forEach(function(particle) {
             particle.draw();
         });
     }
 
-    // Update the scene
     function update() {
         particles.forEach(function(particle) {
             particle.update();
@@ -169,17 +131,16 @@ $.GUI().create('Misty', function(G) {
 
     return {
         fn: function() {
-            // Initialize the scene
+
             init();
 
-            // If the context is set then we can draw the scene (if not then the browser does not support canvas)
             if (context) {
                 setInterval(function() {
-                    // Update the scene befoe drawing
+
                     update();
 
-                    // Draw the scene
                     draw();
+
                 }, 1000 / targetFPS);
             }
         },
